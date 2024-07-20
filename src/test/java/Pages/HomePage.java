f5090cda95a967f2c3f698f3550241da7dc67ed4
gokayari <gokayari@gmail.com>
2024-07-20 18:50:27 +0200

    HomePage

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,4 +1,32 @@
 package Pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class HomePage extends BasePage{
+
+    By benutzernameBar = By.name("username");
+    By passwortBar = By.name("password");
+    By loginTaste = By.cssSelector(".oxd-button");
+
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    public void benutzernameEingeben (String benutzername){
+        wait.until(ExpectedConditions.presenceOfElementLocated(benutzernameBar));
+        driver.findElement(benutzernameBar).sendKeys(benutzername);
+    }
+
+    public void passwortEingeben (String passwort){
+        driver.findElement(passwortBar).sendKeys(passwort);
+    }
+
+    public void einloggen (){
+        driver.findElement(loginTaste).click();
+    }
+
 }
