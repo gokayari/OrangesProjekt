7b8127c02b595f785a14fb79186834eaa7d5d675
gokayari <gokayari@gmail.com>
2024-07-20 18:49:45 +0200

    Testklasse /loginTest

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,8 +1,10 @@
 import Pages.BasePage;
 import Pages.HomePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
 
 public class Tests {
 
@@ -12,12 +14,24 @@ public class Tests {
 
     @BeforeMethod
     public void setup(){
-
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(ConfigurationReader.getProperty("baseUrl"));
+        basePage = new BasePage(driver);
+        homePage = new HomePage(driver);
     }
 
     @Test
     public void loginTest(){
 
+        //Benutzername eingeben:
+        homePage.benutzernameEingeben(ConfigurationReader.getProperty("Benutzername"));
+
+        //Passwort eingeben:
+        homePage.passwortEingeben(ConfigurationReader.getProperty("Passwort"));
+
+        //Einloggen:
+        homePage.einloggen();
     }
 
 }
