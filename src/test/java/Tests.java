e0353d10eef10f29c3f68acafda4fb2122841b36
gokayari <gokayari@gmail.com>
2024-07-20 16:59:42 +0200

    Testklasse

---
+++ src/test/java/Tests.java
@@ -0,0 +1,23 @@
+import Pages.BasePage;
+import Pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    WebDriver driver;
+    BasePage basePage;
+    HomePage homePage;
+
+    @BeforeMethod
+    public void setup(){
+
+    }
+
+    @Test
+    public void loginTest(){
+
+    }
+
+}
