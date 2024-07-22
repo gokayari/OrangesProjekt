3fb02b9afb0b4682e2ec604e66713f2167df4736
gokayari <gokayari@gmail.com>
2024-07-22 23:14:20 +0200

    Testklasse / menüpunkteTest und funktionTest

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,8 +1,11 @@
 import Pages.BasePage;
-import Pages.DashboardPage;
 import Pages.HomePage;
+import Pages.MenuItemsPage;
+import Pages.RecruitmentPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
@@ -12,7 +15,8 @@ public class Tests {
     WebDriver driver;
     BasePage basePage;
     HomePage homePage;
-    DashboardPage dashboardPage;
+    MenuItemsPage menuItemsPage;
+    RecruitmentPage recruitmentPage;
 
     @BeforeMethod
     public void setup(){
@@ -21,11 +25,11 @@ public class Tests {
         driver.get(ConfigurationReader.getProperty("baseUrl"));
         basePage = new BasePage(driver);
         homePage = new HomePage(driver);
+        menuItemsPage = new MenuItemsPage(driver);
     }
 
     @Test
-    public void loginTest(){
-        dashboardPage = new DashboardPage(driver);
+    public void loginTest() {
 
         //Benutzername eingeben:
         homePage.benutzernameEingeben(ConfigurationReader.getProperty("Benutzername"));
@@ -37,7 +41,64 @@ public class Tests {
         homePage.einloggen();
 
         //Prüfen, ob Einloggen erfolgreich war:
-        dashboardPage.einloggenÜberprüfen(ConfigurationReader.getProperty("Überprüfungstext"));
+        menuItemsPage.einloggenÜberprüfen(ConfigurationReader.getProperty("Überprüfungstext"));
+    }
+
+    @Test
+    public void menüpunkteTest(){
+
+        //Benutzername eingeben:
+        homePage.benutzernameEingeben(ConfigurationReader.getProperty("Benutzername"));
+
+        //Passwort eingeben:
+        homePage.passwortEingeben(ConfigurationReader.getProperty("Passwort"));
+
+        //Einloggen:
+        homePage.einloggen();
+
+        //Prüfen, ob andere Menüpunkte geöffnet werden können:
+        menuItemsPage.menüpunkteÜberprüfen();
+
+    }
+
+    @Test
+    public void funktionTest(){
+        recruitmentPage = new RecruitmentPage(driver);
+
+        //Benutzername eingeben:
+        homePage.benutzernameEingeben(ConfigurationReader.getProperty("Benutzername"));
+
+        //Passwort eingeben:
+        homePage.passwortEingeben(ConfigurationReader.getProperty("Passwort"));
+
+        //Einloggen:
+        homePage.einloggen();
+
+        //Prüfen, ob andere Menüpunkte geöffnet werden können:
+        menuItemsPage.menüpunkteÜberprüfen();
+
+        //Eingabe von Kandidatendaten:
+        recruitmentPage.eingabeVonKandidatendaten(ConfigurationReader.getProperty("Vorname"),
+                ConfigurationReader.getProperty("Nachname"),
+                ConfigurationReader.getProperty("Stellenausschreibung"),
+                ConfigurationReader.getProperty("E-Mail"),
+                ConfigurationReader.getProperty("Telefonnumer"),
+                ConfigurationReader.getProperty("Datum"));
+
+        //Prüfen, ob der/die Kandidat/Kandidatin gespeichert wurde:
+        recruitmentPage.kandidatencheck(ConfigurationReader.getProperty("Vorname"),
+                ConfigurationReader.getProperty("Nachname"));
+    }
+
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        driver.quit();
     }
 
 }
